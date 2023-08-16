576b72eb368e8d93fc0dffe79a0e0626371df4ef
Saba <sabarht@gmail.com>
2023-08-16 11:12:59 +0200

    repo for katas

---
+++ kata1/kata1.js
@@ -0,0 +1,24 @@
+// // http://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+// function removal1(str) {
+//   return str.slice(1, -1);
+// }
+
+// const example = "1,5,7,3";
+// function removal2(str) {
+//   let result = str.split(",");
+//   if (result.length > 2) {
+//     return result.slice(1, -1).toString().replaceAll(",", " ");
+//     // .replaceAll(",", " ");
+//   } else return null;
+// }
+
+// console.log(removal2(example));
+
+const array = [1, 1, 2];
+function distinct(a) {
+  array.map((index) => {
+    array.filter(index);
+  });
+}
+
+console.log(distinct(array));
